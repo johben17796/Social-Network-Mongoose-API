9989a981aa909d80819f10545f2649a9da802e9c
Ben Johnson <johben17796@gmail.com>
2025-02-09 16:11:53 -0600

    just need to record and readme :)

--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -16,7 +16,9 @@ router.route('/:thoughtId')
     .get(getSingleThought).delete(deleteThought).put(updateThought);
 
 
+router.route('/:thoughtId/reactions').post(createReaction);
 
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 
